f99ee0997f8f3157ccc18ce0c76aef9fa77f7e71
vjp12 <vijayprabakaran12@gmail.com>
2016-05-10 13:27:33 -0400

    jar

--- CIS 450/src/Executable.java
+++ CIS 450/src/Executable.java
@@ -5,24 +5,21 @@ import javax.json.JsonObject;
 public class Executable {
 	//runs the search engine
 	public static void main(String[] args) throws Exception {
-		AmazonDynamoDB.createTable();
-		Indexer.add("1","1");
-		Indexer.add("1","1");
 		//takes in user and query args
-//		if(args.length != 2){
-//			System.out.println("invalid number of arguments");
-//		} else {
-//			//initializes DynamoDB connection
-//			AmazonDynamoDB.createTable();
-//			//creates search engine
-//			SearchEngine engine = new SearchEngine(args[0]);
-//			//encodes search engine results as jason file
-//			JsonObject obj = JsonEncode.encode(engine.search(args[1]));
-//			try (FileWriter file = new FileWriter("querry.json")) {
-//				file.write(obj.toString());
-//				System.out.println("Successfully Copied JSON Object to File...");
-//				System.out.println("\nJSON Object: " + obj);
-//			}
-//		}
+		if(args.length != 2){
+			System.out.println("invalid number of arguments");
+		} else {
+			//initializes DynamoDB connection
+			AmazonDynamoDB.createTable();
+			//creates search engine
+			SearchEngine engine = new SearchEngine(args[0]);
+			//encodes search engine results as jason file
+			JsonObject obj = JsonEncode.encode(engine.search(args[1]));
+			try (FileWriter file = new FileWriter("querry.json")) {
+				file.write(obj.toString());
+				System.out.println("Successfully Copied JSON Object to File...");
+				System.out.println("\nJSON Object: " + obj);
+			}
+		}
 	}
 }
